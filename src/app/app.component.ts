cf54b954382f8268c3993c9e9cba566a05f6e0b1
Paul-rs1 <paul.epp@rohde-schwarz.com>
2022-06-30 11:02:26 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-})
-export class AppComponent {
-  nestedObject = {
+function getNewObject() {
+  return {
     name: 'Mustermann',
     age: 43,
     updated: new Date(),
@@ -17,12 +12,21 @@ export class AppComponent {
       updated: new Date(),
     },
   };
-  lastUpdated = this.nestedObject.updated;
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  nestedObject = getNewObject();
+
+  constructor(private cdref: ChangeDetectorRef) {}
 
   updateObject() {
     const updated = new Date();
     this.nestedObject.updated = updated;
     this.nestedObject.information.updated;
-    this.lastUpdated = updated;
   }
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,8 +2,15 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<div>{{nestedObject.information | json}}</div>`,
-  styles: [`div { font-family: Lato; background-color:pink }`],
+  template: `
+    <div>Updated: {{nestedObject.updated.getTime()}}</div>
+    <div>Nested updated: {{nestedObject.information.updated.getTime()}}</div> 
+  `,
+  styles: [
+    `
+      :div { background-color: pink; }
+    `,
+  ],
 })
 export class HelloComponent {
   @Input() nestedObject;
